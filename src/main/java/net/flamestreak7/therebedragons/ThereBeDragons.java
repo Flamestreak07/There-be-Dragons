0bd71d6e9386c340942f4d38b0239294b5802974
Flamestreak7 <Struan.Harrild@gmail.com>
2024-06-02 21:11:31 +1200

    FIRST ITEM

--- src/main/java/net/flamestreak7/therebedragons/ThereBeDragons.java
+++ src/main/java/net/flamestreak7/therebedragons/ThereBeDragons.java
@@ -2,18 +2,18 @@ package net.flamestreak7.therebedragons;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.flamestreak7.therebedragons.items.Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ThereBeDragons implements ModInitializer {
+public class ThereBeDragons implements ModInitializer
+{
 	public static final String MOD_ID = "there-be-dragons";
     public static final Logger LOGGER = LoggerFactory.getLogger("there-be-dragons");
 
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-
-		LOGGER.info("Hello Fabric world!");
+	public void onInitialize()
+	{
+		Items.registerModItems();
 	}
 }
---
+++ src/main/java/net/flamestreak7/therebedragons/items/Items.java
@@ -0,0 +1,33 @@
+package net.flamestreak7.therebedragons.items;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.flamestreak7.therebedragons.ThereBeDragons;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class Items
+{
+    public static final Item FirstItem = registerItem("firstitem", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries)
+    { // This adds shit to the creative menu, specifically the ingredients tab
+        entries.add(FirstItem);
+    }
+
+
+    private static Item registerItem(String name, Item item)
+    { // This makes them a unique item
+        return Registry.register(Registries.ITEM, new Identifier(ThereBeDragons.MOD_ID, name), item);
+    }
+    public static void registerModItems()
+    { // This registers them as an item
+        ThereBeDragons.LOGGER.info("Registering mod items for " + ThereBeDragons.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(Items ::addItemsToIngredientItemGroup);
+    }
+}
